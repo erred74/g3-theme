9e27abd3d6f018ea234db7b1142330e006a4430a
danielraison@gmail.com <danielraison@gmal.com>
2021-02-15 09:39:19 +0100

    woocommerce setup

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,131 @@
 // Styles
 import '../sass/main.scss';
+
+jQuery(document).ready(function ($) {
+    gsap.set("#new", {
+        autoAlpha: 0
+    })
+
+    var $button = $('.menu__item-link'),
+        $page = $('.screen__item'),
+        $screens = $('.screens');
+
+    $button.on('mouseenter', function (e) {
+        var $thisPage = $(this).attr('id');
+
+        gsap.to($thisPage, 0.5, {
+            autoAlpha: 1
+        });
+        gsap.to($page.not($thisPage), 0.5, {
+            autoAlpha: 0
+        }); // faster
+    });
+
+    $button.on('mouseleave', function (e) {
+        var $thisPage = $(this).attr('id');
+
+        gsap.to($page, 0.3, {
+            autoAlpha: 0
+        });
+    });
+
+    // Translate header up on scroll
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 40) {
+            $('.site-header').addClass('scrolled');
+        } else {
+            $('.site-header').removeClass('scrolled');
+        }
+    });
+
+    // bind nav resize
+
+    $(window).bind("resize", function () {
+        if ($(this).width() < 768) {
+            $('.site-header').addClass('overlay__nav');
+        } else {
+            $('.site-header').removeClass('overlay__nav');
+        }
+    }).trigger('resize');
+
+    // Hamburger
+
+    var trigger = $('#hamburger'),
+        isClosed = true,
+        closeNav = $('#closeNav');
+
+    trigger.click(function () {
+        burgerTime();
+    });
+
+    closeNav.click(function () {
+        burgerClose();
+    });
+
+    function burgerTime() {
+        if (isClosed == false) {
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            $('.nav__wrapper').removeClass('is-open');
+            isClosed = true;
+        } else {
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            $('.nav__wrapper').addClass('is-open');
+            isClosed = false;
+        }
+    }
+
+    function burgerClose() {
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        $('.nav__wrapper').removeClass('is-open');
+        isClosed = true;
+    }
+
+    function floatLabel(inputType){
+		$(inputType).each(function(){
+			var $this = $(this);
+			var text_value = $(this).val();
+
+			// on focus add class "active" to label
+			$this.focus(function(){
+				$this.next().addClass("active");
+			});
+
+			// on blur check field and remove class if needed
+			$this.blur(function(){
+				if($this.val() === '' || $this.val() === 'blank'){
+					$this.next().removeClass();
+					}
+			});
+					
+			// Check input values on postback and add class "active" if value exists
+			if(text_value!=''){
+				$this.next().addClass("active");
+				}
+			});
+    
+    	// Automatically remove floatLabel class from select input on load
+    	$( "select" ).next().removeClass();
+	}
+	// Add a class of "floatLabel" to the input field
+	floatLabel(".floatLabel");
+});
+
+const reveal = gsap.utils.toArray('.reveal');
+reveal.forEach((text, i) => {
+    ScrollTrigger.create({
+        trigger: text,
+        toggleClass: 'active',
+        start: "top 90%",
+        scrub: true,
+        once: true
+    });
+})
+
+window.addEventListener('load', function(){
+    gsap.registerPlugin(ScrollTrigger);
+    ScrollTrigger.refresh();
+})
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,29 @@
  * Webpack configuration.
  */
 
-const path = require( 'path' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
-const cssnano = require( 'cssnano' ); // https://cssnano.co/
-const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
-const UglifyJsPlugin = require( 'uglifyjs-webpack-plugin' );
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const cssnano = require('cssnano'); // https://cssnano.co/
+const {
+	CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin'); // https://webpack.js.org/plugins/copy-webpack-plugin/
-const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 // JS Directory path.
-const JS_DIR = path.resolve( __dirname, 'src/js' );
-const IMG_DIR = path.resolve( __dirname, 'src/img' );
-const LIB_DIR = path.resolve( __dirname, 'src/library' );
-const BUILD_DIR = path.resolve( __dirname, 'build' );
+const JS_DIR = path.resolve(__dirname, 'src/js');
+const IMG_DIR = path.resolve(__dirname, 'src/img');
+const LIB_DIR = path.resolve(__dirname, 'src/library');
+const BUILD_DIR = path.resolve(__dirname, 'build');
 
 const entry = {
 	main: JS_DIR + '/main.js',
 	single: JS_DIR + '/single.js',
 	editor: JS_DIR + '/editor.js',
 	blocks: JS_DIR + '/blocks.js',
+	vendors: JS_DIR + '/vendors.js',
 };
 
 const output = {
@@ -32,31 +35,31 @@ const output = {
 /**
  * Note: argv.mode will return 'development' or 'production'.
  */
-const plugins = ( argv ) => [
-	new CleanWebpackPlugin( {
-		cleanStaleWebpackAssets: ( 'production' === argv.mode  ) // Automatically remove all unused webpack assets on rebuild, when set to true in production. ( https://www.npmjs.com/package/clean-webpack-plugin#options-and-defaults-optional )
-	} ),
+const plugins = (argv) => [
+	new CleanWebpackPlugin({
+		cleanStaleWebpackAssets: ('production' === argv.mode) // Automatically remove all unused webpack assets on rebuild, when set to true in production. ( https://www.npmjs.com/package/clean-webpack-plugin#options-and-defaults-optional )
+	}),
 
-	new MiniCssExtractPlugin( {
+	new MiniCssExtractPlugin({
 		filename: 'css/[name].css'
-	} ),
+	}),
 
-	new CopyPlugin( {
-		patterns: [
-			{ from: LIB_DIR, to: BUILD_DIR + '/library' }
-		]
-	} ),
+	new CopyPlugin({
+		patterns: [{
+			from: LIB_DIR,
+			to: BUILD_DIR + '/library'
+		}]
+	}),
 
-	new DependencyExtractionWebpackPlugin( {
+	new DependencyExtractionWebpackPlugin({
 		injectPolyfill: true,
 		combineAssets: true,
-	} )
+	})
 ];
 
-const rules = [
-	{
+const rules = [{
 		test: /\.js$/,
-		include: [ JS_DIR ],
+		include: [JS_DIR],
 		exclude: /node_modules/,
 		use: 'babel-loader'
 	},
@@ -81,7 +84,7 @@ const rules = [
 	},
 	{
 		test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-		exclude: [ IMG_DIR, /node_modules/ ],
+		exclude: [IMG_DIR, /node_modules/],
 		use: {
 			loader: 'file-loader',
 			options: {
@@ -102,7 +105,7 @@ const rules = [
  *
  * @see https://webpack.js.org/configuration/configuration-types/#exporting-a-function
  */
-module.exports = ( env, argv ) => ({
+module.exports = (env, argv) => ({
 
 	entry: entry,
 
@@ -121,19 +124,19 @@ module.exports = ( env, argv ) => ({
 
 	optimization: {
 		minimizer: [
-			new OptimizeCssAssetsPlugin( {
+			new OptimizeCssAssetsPlugin({
 				cssProcessor: cssnano
-			} ),
+			}),
 
-			new UglifyJsPlugin( {
+			new UglifyJsPlugin({
 				cache: false,
 				parallel: true,
 				sourceMap: false
-			} )
+			})
 		]
 	},
 
-	plugins: plugins( argv ),
+	plugins: plugins(argv),
 
 	externals: {
 		jquery: 'jQuery'
